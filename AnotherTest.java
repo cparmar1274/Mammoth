b995074b5f654b5386cf7efcfb6e51e5ba4b78d7
Chirag Parmar <cparmar@sprypoint.com>
2015-05-04 15:16:17 -0300

    Mammoth Commits.

---
+++ AnotherTest.java
@@ -0,0 +1,13 @@
+package com.company.Mammoth;
+
+/**
+ * Created by cparmar on 15-05-04.
+ */
+public class AnotherTest {
+
+    public static void main(Sting[] st)
+    {
+        String test = "Finish Test";
+
+    }
+}
