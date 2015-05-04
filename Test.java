337fbe24864eb4eca375688887010a48f3699ce5
Chirag Parmar <cparmar@sprypoint.com>
2015-05-04 14:34:10 -0300

    Mammoth Commits.

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.company.Mammoth;
+
+/**
+ * Created by cparmar on 15-05-04.
+ */
+public class Test {
+    public static void main(String[] args)
+    {
+        System.out.print("First Mammoth Born.");
+    }
+}
